cfc1067b5feb7fcb3ccf640698ab61b036d5c192
David Passaro <davepassaro@yahoo.com>
2022-09-07 15:49:33 -0700

    table one working without filter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect, useContext} from 'react';
+import Table from "./Table";
+
+const FilterContext = React.createContext([]);
+
+function useContextState() {
+  const context = useContext(FilterContext);
+  if (context === undefined) {
+    throw new Error(
+      "useTicketDataState must be used within a TicketDataProvider"
+    );
+  }
+  return context;
+}
 
 function App() {
+
+  //const filterContextArray = React.createContext([]);
+  //const [isOpen, setIsOpen] = React.useState(false);
+  //const [tableData, setTableData] = React.useState(false);
+  const [inputValue, setValue] = useState("");
+  const [tableOneIndex,setTableOneIndex] = useState({start: 1, end: 1});
+  const [tableTwoIndex,setTableTwoIndex] = useState({start: 1, end: 1});
+  const [APIData, setAPIData] = useState();
+
+  
+  useEffect(() => {
+    (async function() {
+      try {
+        const response = await fetch(
+            'https://jsonplaceholder.typicode.com/todos'
+        );
+        const json = await response.json();
+        setAPIData(json)//json.data.map(it => it.data));
+      } catch (e) {
+        console.error(e);
+      }
+    })();
+  }, []);
+
+
+
+
+  // React.useEffect(()=>{
+  //   async function fetchMyAPI() {
+  //     let response = await fetch('https://jsonplaceholder.typicode.com/todos');
+  //     response = await response.json();
+  //     console.log(response)
+  //     setTableData(response);
+  //   }
+  //   fetchMyAPI();
+  //   console.log( tableData, 'td')
+  // },[]);
+
+  // function toggleAppOpen() {
+  //   setIsOpen(prevIsOpen => !prevIsOpen);
+  // }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React Test
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div>
+      { 
+      /* {toggleTable ?
+          <appContext.Provider value={[filterContextArray]}>
+          {children}
+        </appContext.Provider> :
+        <div className='startAppButtonParent'><button id="openButton" onClick={toggleAppOpen}>Switch Tables</button></div> */
+        }
+      </div>
+
+      <FilterContext.Provider value={
+        [
+          tableOneIndex, setTableOneIndex, 
+          tableTwoIndex, setTableTwoIndex, 
+          APIData, setAPIData
+        ]
+        }>
+        <Table />
+      </FilterContext.Provider>
     </div>
   );
 }
 
-export default App;
+export {
+  App,
+  useContextState
+};
---
+++ src/Table.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { useContextState } from "./App";
+
+export default function Table() {
+
+    const [tableOneIndex, setTableOneIndex, tableTwoIndex, setTableTwoIndex, APIData, setAPIData] = useContextState();
+
+    const handleSubmit = e => {
+      e.preventDefault();
+      console.log(e.target[0].value,e.target[1].value, 'hsub')
+      setTableOneIndex({
+        start:e.target[0].value, 
+        end: e.target[1].value
+      })
+    };
+
+  return (
+    <div>
+    <form onSubmit={handleSubmit}>
+    <span>Starting Index</span>
+    <input value={tableOneIndex.start} onChange={
+      e => {
+      setTableOneIndex(tableOneIndex =>(
+          {...tableOneIndex, start: e.target.value}
+        ))
+      }} />
+    <span>Ending Index</span>
+    <input value={tableOneIndex.end} onChange={
+      e => {
+      setTableOneIndex(tableOneIndex =>(
+          {...tableOneIndex,  end: e.target.value}
+        ))
+      }} />
+      <button>Submit</button>
+  </form>
+    <table className="table">
+    <tbody>
+      <tr>
+        <th className="table-header">ID</th>
+        <th >Title</th>
+      </tr>
+        {APIData && APIData.map(item => (
+        <tr key={item.id}>
+          <td> {item.id}</td>
+          <td> {item.title} </td>
+      </tr>
+      ))}
+      </tbody>
+    </table>
+    </div>
+  );
+}
